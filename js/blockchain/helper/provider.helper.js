b63388354f5d98f4a726f74643935674b0bf2d59
shaharyar saleem <shaharyarsaleem71@gmail.com>
2021-12-08 23:54:55 +0500

    style: run format

--- js/blockchain/helper/provider.helper.js
+++ js/blockchain/helper/provider.helper.js
@@ -5,31 +5,31 @@ let etherProvider;
 let signer;
 
 function getReadOnlyProvider(rpcUrl = RPC_URL[56]) {
-    if (rpcProvider) {
-        return rpcProvider;
-    }
-    rpcProvider = new ethers.providers.JsonRpcProvider(RPC_URL[56]);
+  if (rpcProvider) {
     return rpcProvider;
+  }
+  rpcProvider = new ethers.providers.JsonRpcProvider(RPC_URL[56]);
+  return rpcProvider;
 }
 function getWeb3Provider() {
-    if (!window.ethereum) {
-        return null;
-    }
-    web3Provider = new ethers.providers.Web3Provider(window.ethereum);
-    return web3Provider;
+  if (!window.ethereum) {
+    return null;
+  }
+  web3Provider = new ethers.providers.Web3Provider(window.ethereum);
+  return web3Provider;
 }
 function getSigner() {
-    if (signer) {
-        return signer;
-    }
-    etherProvider = getProvider();
-    signer = etherProvider.getSigner();
+  if (signer) {
     return signer;
+  }
+  etherProvider = getProvider();
+  signer = etherProvider.getSigner();
+  return signer;
 }
 function getProvider() {
-    return getWeb3Provider() || getReadOnlyProvider();
+  return getWeb3Provider() || getReadOnlyProvider();
 }
 module.exports = {
-    getProvider,
-    getSigner,
+  getProvider,
+  getSigner,
 };
