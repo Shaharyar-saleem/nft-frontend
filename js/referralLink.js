2d01a41bc96639875542f3c010c02f53b00fbf56
shaharyar saleem <shaharyarsaleem71@gmail.com>
2021-12-25 00:15:01 +0500

    fix(referral): icap used in referral link

--- js/referralLink.js
+++ js/referralLink.js
@@ -1,27 +1,38 @@
-const {ZERO_ADDRESS} = require("./blockchain/constants");
-const {providerHelper} = require("./blockchain/helper/index");
+const {
+  ZERO_ADDRESS,
+  REFERRAL_QUERY_PARAM,
+  REFERRAL_COOKIE_NAME,
+} = require("./blockchain/constants");
+const { providerHelper } = require("./blockchain/helper/index");
 const signer = providerHelper.getSigner();
 
-const params = new URLSearchParams(window.location.search);
-if (params.has('ref')){
-    setRefCookie("ref", params.get('ref'), 30);
+userReferralLink();
+getRef();
+$('[data-toggle="tooltip"]').click(function () {
+  $(this).tooltip("hide").attr("data-original-title", "Copied").tooltip("show");
+});
+
+function getRef() {
+  const params = new URLSearchParams(window.location.search);
+  if (params.has(REFERRAL_QUERY_PARAM)) {
+    setCookie(REFERRAL_COOKIE_NAME, params.get(REFERRAL_QUERY_PARAM), 30);
+  }
 }
 
-function setRefCookie(cname,cvalue,exdays) {
-    const date = new Date();
-    date.setTime(date.getTime() + (exdays*24*60*60*1000));
-    let expires = "expires=" + date.toGMTString();
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+function setCookie(name, value, expiryInDays) {
+  const date = new Date();
+  date.setTime(date.getTime() + expiryInDays * 24 * 60 * 60 * 1000);
+  let expires = "expires=" + date.toGMTString();
+  document.cookie = name + "=" + value + ";" + expires + ";path=/";
 }
-
 
 /**
-* Is the string a valid address
-* @param address
-* @returns boolean
-*/
+ * Is the string a valid address
+ * @param address
+ * @returns boolean
+ */
 function isValidAddress(address) {
-    return ethers.utils.isAddress(address)
+  return ethers.utils.isAddress(address)
 }
 
 /**
@@ -30,7 +41,7 @@ function isValidAddress(address) {
  * @returns string
  */
 function convertFromIcap(address) {
-    return isValidAddress(address) ? ethers.utils.getAddress(address) : null
+  return isValidAddress(address) ? ethers.utils.getAddress(address) : null
 }
 
 /**
@@ -39,36 +50,37 @@ function convertFromIcap(address) {
  * @returns string
  */
 function convertToIcap(address) {
-    return isValidAddress(address) ? ethers.utils.getIcapAddress(address) : null
+  return isValidAddress(address) ? ethers.utils.getIcapAddress(address) : null
 }
 
-async function userReferralLink(){
-    const userAddress = await signer.getAddress();
-    const icapAddress = convertToIcap(userAddress);
-    const referralLink = `https://nft.fuzion.team/?ref=${icapAddress}`;
-    const referralElement = document.getElementById("userReferralLink");
-    if (referralElement){
-        referralElement.innerText = referralLink;
-        document.getElementById("referralLink").value = referralLink;
-    }
-    return referralLink;
+async function userReferralLink() {
+  const userAddress = await signer.getAddress();
+  const icapAddress = convertToIcap(userAddress);
+  const referralLink = `https://nft.fuzion.team/?ref=${icapAddress}`;
+  const referralElement = document.getElementById("userReferralLink");
+  if (referralElement) {
+    referralElement.innerText = referralLink;
+    document.getElementById("referralLink").value = referralLink;
+  }
+  return referralLink;
 }
 
 function copyReferralLink() {
-    const copyText = document.getElementById("referralLink");
-    copyText.select();
-    copyText.setSelectionRange(0, 99999);
-    navigator.clipboard.writeText(copyText.value);
+  const copyText = document.getElementById("referralLink");
+  copyText.select();
+  copyText.setSelectionRange(0, 99999);
+  navigator.clipboard.writeText(copyText.value);
 }
 
-$('[data-toggle="tooltip"]').click(function () {
-    $(this).tooltip("hide").attr("data-original-title", "Copied").tooltip("show");
-});
+function getReferral() {
+  // let ref;
+  // grab the referral cookie value
+  // return convertToIcap(ref) || ZERO_ADDRESS;
+}
 
-userReferralLink();
 window.copyReferralLink = copyReferralLink;
-
 module.exports = {
-    userReferralLink,
-    copyReferralLink,
-}
+  userReferralLink,
+  copyReferralLink,
+  getRef,
+};
