ae9d909a343b385dae4df7d80dd7cf8305868183
shaharyar saleem <shaharyarsaleem71@gmail.com>
2021-12-24 23:58:21 +0500

    fix: referral link fixed

--- js/referralLink.js
+++ js/referralLink.js
@@ -1,61 +1,74 @@
-const {
-  ZERO_ADDRESS,
-  REFERRAL_QUERY_PARAM,
-  REFERRAL_COOKIE_NAME,
-} = require("./blockchain/constants");
-const { providerHelper } = require("./blockchain/helper/index");
+const {ZERO_ADDRESS} = require("./blockchain/constants");
+const {providerHelper} = require("./blockchain/helper/index");
 const signer = providerHelper.getSigner();
 
-userReferralLink();
-getRef();
-$('[data-toggle="tooltip"]').click(function () {
-  $(this).tooltip("hide").attr("data-original-title", "Copied").tooltip("show");
-});
-
-function getRef() {
-  const params = new URLSearchParams(window.location.search);
-  if (params.has(REFERRAL_QUERY_PARAM)) {
-    setCookie(REFERRAL_COOKIE_NAME, params.get(REFERRAL_QUERY_PARAM), 30);
-  }
+const params = new URLSearchParams(window.location.search);
+if (params.has('ref')){
+    setRefCookie("ref", params.get('ref'), 30);
 }
 
-function setCookie(name, value, expiryInDays) {
-  const date = new Date();
-  date.setTime(date.getTime() + expiryInDays * 24 * 60 * 60 * 1000);
-  let expires = "expires=" + date.toGMTString();
-  document.cookie = name + "=" + value + ";" + expires + ";path=/";
+function setRefCookie(cname,cvalue,exdays) {
+    const date = new Date();
+    date.setTime(date.getTime() + (exdays*24*60*60*1000));
+    let expires = "expires=" + date.toGMTString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
 
-async function userReferralLink() {
-  const userAddress = await signer.getAddress();
-  const addressString = userAddress.toString();
-  const icapAddress = addressString.toUpperCase();
-  const referralLink = `https://nft.fuzion.team/?ref=${icapAddress}`;
-  // const defaultReferralLink = `https://nft.fuzion.team/?ref=${ZERO_ADDRESS}`;
-  const referralElement = document.getElementById("userReferralLink");
-  if (referralElement) {
-    referralElement.innerText = referralLink;
-    document.getElementById("referralLink").value = referralLink;
-  }
-  return referralLink;
+
+/**
+* Is the string a valid address
+* @param address
+* @returns boolean
+*/
+function isValidAddress(address) {
+    return ethers.utils.isAddress(address)
+}
+
+/**
+ * Get the normal address value
+ * @param address
+ * @returns string
+ */
+function convertFromIcap(address) {
+    return isValidAddress(address) ? ethers.utils.getAddress(address) : null
+}
+
+/**
+ * Convert normal address to icap address
+ * @param address
+ * @returns string
+ */
+function convertToIcap(address) {
+    return isValidAddress(address) ? ethers.utils.getIcapAddress(address) : null
+}
+
+async function userReferralLink(){
+    const userAddress = await signer.getAddress();
+    const icapAddress = convertToIcap(userAddress);
+    const referralLink = `https://nft.fuzion.team/?ref=${icapAddress}`;
+    const referralElement = document.getElementById("userReferralLink");
+    if (referralElement){
+        referralElement.innerText = referralLink;
+        document.getElementById("referralLink").value = referralLink;
+    }
+    return referralLink;
 }
 
 function copyReferralLink() {
-  const copyText = document.getElementById("referralLink");
-  copyText.select();
-  copyText.setSelectionRange(0, 99999);
-  navigator.clipboard.writeText(copyText.value);
+    const copyText = document.getElementById("referralLink");
+    copyText.select();
+    copyText.setSelectionRange(0, 99999);
+    navigator.clipboard.writeText(copyText.value);
 }
 
-function getReferral() {
-  // let ref;
-  // grab the referral cookie value
-  // return convertToIcap(ref) || ZERO_ADDRESS;
-}
+$('[data-toggle="tooltip"]').click(function () {
+    $(this).tooltip("hide").attr("data-original-title", "Copied").tooltip("show");
+});
 
+userReferralLink();
 window.copyReferralLink = copyReferralLink;
+
 module.exports = {
-  userReferralLink,
-  copyReferralLink,
-  getRef,
-};
+    userReferralLink,
+    copyReferralLink,
+}
