c6df6a12006f02680ce75e78f3584cfc55f918aa
shaharyar saleem <shaharyarsaleem71@gmail.com>
2021-12-25 23:05:52 +0500

    fix: modals.js code cleaned-up

--- js/modal.js
+++ js/modal.js
@@ -3,23 +3,3 @@ const span = document.getElementsByClassName("close")[0];
 const closeTransactionModal = document.getElementById("closeTransactionModal");
 const transactionModal = document.getElementById("transactionModal");
 
-span.onclick = function () {
-    modal.style.display = "none";
-};
-window.onclick = function (event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-    if (event.target == processingModal) {
-        processingModal.style.display = "none";
-    }
-    if (event.target == confirmationModal) {
-        confirmationModal.style.display = "none";
-    }
-    if (event.target == transactionModal) {
-        transactionModal.style.display = "none";
-    }
-};
-closeTransactionModal.onclick = function () {
-    transactionModal.style.display = "none";
-};
